c0436e28985012ba0edd6b420992ce1aff571340
Victor Paltz <paltz.victor@free.fr>
2020-02-03 04:24:02 +0100

    comment infinity

--- Alert.py
+++ Alert.py
@@ -1,8 +1,22 @@
 from utils import format_time
 
+
 class Alert:
+    """
+    Class that defines Alert objects
+    """
 
     def __init__(self, start_time, hits, end_time=None):
+        """
+        Parameters
+        ----------
+        start_time : float
+            Date when the alert was triggered in seconds
+        hits : float
+            Value of the average traffic that first exceeded the threshold
+        end_time : float or None
+            Date of the end of the alert in seconds (if the alert was resolved)
+        """
 
         self.start_time = start_time
         self.hits = hits
@@ -11,20 +25,32 @@ class Alert:
         if end_time is not None:
             self.resolved = True
             self.end_time = end_time
-        
 
     def resolve(self, end_time):
+        """
+        Change the status of an alert from not resolved to resolved
+
+        Parameters
+        ----------
+        end_time : float
+            Date of the end of the alert in seconds 
+        """
+
         self.resolved = True
         self.end_time = end_time
 
     def report(self):
+        """
+        generate a report on the alert
+        """
+
         report = ""
         report += f"- /!\ High traffic generated an alert - hits {self.hits:.2f} req/s, "
         report += f"triggered at time {format_time(self.start_time)}\n"
         if self.resolved:
-            report += f"- End of alert, "
+            report += f"      └─> End of alert, "
             report += f"recovered at time {format_time(self.end_time)}\n"
         else:
-            report += "- Alert not recovered\n"
+            report += "      └─> Alert not recovered\n"
 
         return report
